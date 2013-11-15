053beaa4a265a9758fe0a5b57141589d26ea6568
Ali Ebrahim <aebrahim@ucsd.edu>
2013-11-14 16:04:24 -0800

    travis config in its own script

---
+++ .travis_prepare.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd $HOME # home should be in /home/travis
+
+if [ "$JYTHON" = "true" ]; then
+    sudo apt-get install libglpk-java
+    wget http://clostridium.ucsd.edu/jython-installer-2.7-b1.jar
+    wget http://clostridium.ucsd.edu/jsbml-0.8-with-dependencies.jar
+    export JYTHONPATH="/usr/share/java/glpk-java:jsbml-0.8-with-dependencies.jar"
+    java -jar jython-installer-2.7-b1.jar -s -d $HOME/jython
+    export PYTHON_EXE=$HOME/jython/jython
+    $PYTHON_EXE -c "print ''"
+fi
+
+if [ "$JYTHON" = "false" ]; then
+    sudo apt-get install libglpk-dev python-scipy libgmp-dev
+    pip install glpk
+    ln -s /usr/lib/python2.7/dist-packages/scipy ~/virtualenv/python2.7/lib/python2.7/site-packages/
+    wget http://clostridium.ucsd.edu/libsbml-5.6.0_built_with_py.tar.gz
+    tar xzf libsbml-5.6.0_built_with_py.tar.gz
+    cd libsbml-5.6.0/
+    sudo make install
+    export PYTHON_EXE=python
+fi
+
+cd $TRAVIS_BUILD_DIR
